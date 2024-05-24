d25982411ca6a035c30fcdbb20388b267965271d
Gina Häußge <gina@octoprint.org>
2024-05-24 15:07:21 +0200

    📝 Add issue tracker to footer
    Also improve footer styling

--- src/assets/cardfoldr.js
+++ src/assets/cardfoldr.js
@@ -1054,10 +1054,9 @@ window.onload = async () => {
     try {
         const versionInfo = await fetch("./version.json");
         const versionData = await versionInfo.json();
-        const version = versionData.commit && versionData.build && versionData.date ? `Build #${versionData.build} of commit ${versionData.commit}` : "Local build";
 
         const versionElement = document.getElementById("version");
-        versionElement.innerHTML = ` &middot; ${version}`;
+        versionElement.innerHTML = versionData.commit && versionData.build && versionData.date ? `Build #${versionData.build} of commit ${versionData.commit}` : "Local build";
     } catch (e) {
         console.error("Error loading version information", e);
     }
