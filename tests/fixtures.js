ab176cf52739cecbb9d3b192f0804296160f8609
Gina Häußge <gina@octoprint.org>
2024-05-25 18:47:14 +0200

    ✅ Test PDF generation
    Only a basic one for now, with default parameters

--- tests/fixtures.js
+++ tests/fixtures.js
@@ -4,6 +4,9 @@ const testPdf = {
     query: "grid-count-x=3&grid-count-y=3&grid-width=40&grid-height=40&grid-start-x=0&grid-start-y=0&grid-margin-x=0&grid-margin-y=0&grid-cut-margin=0",
     preset: {"grid-count-x":"3","grid-count-y":"3","grid-width":"40","grid-height":"40","grid-start-x":"0","grid-start-y":"0","grid-margin-x":"0","grid-margin-y":"0","grid-cut-margin":"0","grid-step-size":"0.1","cards-backs":"fileall","cards-rotate-backs":false,"_key":"test-pdf","_name":"Test PDF"},
     path: "./files/test-pdf.pdf",
+    filename: "test-pdf.pdf",
+    outputPath : "./files/test-pdf.foldable.pdf",
+    outputFilename: "test-pdf.foldable.pdf"
 }
 
 exports.test = base.test.extend({
---
+++ tests/specs/generate-pdf.spec.js
@@ -0,0 +1,57 @@
+const { test, expect } = require('../fixtures');
+const ComparePdf = require("compare-pdf");
+const fs = require("fs");
+
+const pdfCompare = async (actual, expected) => {
+    return await new ComparePdf()   
+        .actualPdfFile(actual)
+        .baselinePdfFile(expected)
+        .compare();
+};
+
+test.beforeEach(async ({page, testPdf}) => {
+    await page.goto(`./?${testPdf.query}`);
+
+    // load test PDF
+    await page.locator("#file").setInputFiles(testPdf.path);
+    await expect(page.locator("#pages > .page")).toHaveCount(3);
+
+    // extract cards
+    await expect(page.locator("#generate")).toBeDisabled();
+    await page.locator("[data-query='cards-backs']").selectOption("lastpage");
+    await page.locator("#extractCards").click();
+    await expect(page.locator("#cards > .card")).toHaveCount(18);
+    await expect(page.locator("#generate")).toBeEnabled();
+});
+
+test("Simple PDF generation", async ({page, testPdf}) => {
+    // generate PDF
+    await page.locator("#generate").click();
+
+    // verify the PDF is generated
+    await expect(await page.locator("#download-button").getAttribute("href")).toContain("blob:");
+    await expect(await page.locator("#output iframe").getAttribute("src")).toContain("blob:");
+
+    // download PDF
+    const downloadPromise = page.waitForEvent('download');
+    await expect(await page.locator("#download-button").getAttribute("download")).toBe(testPdf.outputFilename);
+    await page.locator("#download-button").click();
+    const download = await downloadPromise;
+
+    // verify the PDF is downloaded
+    expect(download.url()).toContain("blob:");
+    expect(download.suggestedFilename()).toMatch(/.pdf$/i);
+
+    let result;
+    try {
+        result = await pdfCompare(await download.path(), testPdf.outputPath);
+    } catch (error) {
+        result = error;
+    }
+    console.log("Result:", result);
+    expect(result.message).toBeUndefined();
+    expect(result.status).toBe("passed")
+    
+    // cleanup
+    await download.delete();
+});
